f41ad3996c5a4cc1b67d693253795cc978c14607
Malinga Peiris <malinga@morbachholdings.com>
2022-09-19 11:50:08 +0530

    19/09/2022 - secondary navbar, tab top space

--- assets/js/custom/Table/pagination.js
+++ assets/js/custom/Table/pagination.js
@@ -13,3 +13,18 @@ $("#kt_datatable_dom_positioning").DataTable({
     "<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
     ">",
 });
+$("#kt_datatable_dom_positioning1").DataTable({
+  language: {
+    lengthMenu: "Show _MENU_",
+  },
+  dom:
+    "<'row'" +
+    "<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+    "<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+    ">" +
+    "<'table-responsive'tr>" +
+    "<'row'" +
+    "<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+    "<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+    ">",
+});
